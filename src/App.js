1a8a34fe63fb603fcc5bd2cd8e53e07b36897f57
Ranel John Tormis <62842207+R-esentful@users.noreply.github.com>
2023-01-31 21:55:28 +0800

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Main Components
 import Headers from './components/Headers';
 import Home from './components/Home';
+import New from './components/New';
 import Footer from './components/Footer';
 
 function App() {
 	return (
-		<>
+		<Router>
 			<Headers />
-			<Home />
+			<div className="container">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/new" element={<New />} />
+				</Routes>
+			</div>
+
 			<Footer />
-		</>
+		</Router>
 	);
 }
 
