ed156d662ea115d5e5795e4aeb949be82c920f03
Ranel John Tormis <62842207+R-esentful@users.noreply.github.com>
2023-01-31 21:55:43 +0800

    Updated Headers

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -5,13 +5,14 @@ import Anchor from './shared/Anchor';
 import Buttons from './shared/Buttons';
 import Image from './shared/Image';
 import Span from './shared/Span';
+import { BrowserRouter as Router, Route, Routes, Link, NavLink } from 'react-router-dom';
 
 function Headers() {
 	return (
 		<nav className="navbar navbar-dark bg-dark navbar-expand-lg">
 			<div className="container-fluid">
 				<Anchor className="navbar-brand">
-					<Image className="d-inline-block align-text-top" src={logo} width="30" height="30" />
+					<Image className="d-inline-block align-text-top" src={logo} width={30} height={30} />
 					Simple React Todo!
 				</Anchor>
 				<Buttons
@@ -27,9 +28,15 @@ function Headers() {
 
 				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
 					<div className="navbar-nav">
-						<Anchor className="nav-link">Home</Anchor>
-						<Anchor className="nav-link">New</Anchor>
-						<Anchor className="nav-link">All task</Anchor>
+						<NavLink to="/" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
+							Home
+						</NavLink>
+						<NavLink to="/new" className="nav-link">
+							New Task
+						</NavLink>
+						<NavLink to="/all" className="nav-link">
+							All Task
+						</NavLink>
 					</div>
 				</div>
 			</div>
