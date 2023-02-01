32ba3ffe513fbd1134cbc30adf96a8aa1eff8424
Ranel John Tormis <62842207+R-esentful@users.noreply.github.com>
2023-02-01 21:59:20 +0800

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
 // Main Components
 import Headers from './components/Headers';
 import Home from './components/Home';
 import New from './components/New';
+import All from './components/All';
 import Footer from './components/Footer';
 
 function App() {
+	// @todo - Move data to useContext
+	const [data, setData] = useState([]);
+
+	const newTask = (task) => {
+		setData([task, ...data]);
+	};
 	return (
 		<Router>
 			<Headers />
 			<div className="container">
 				<Routes>
 					<Route path="/" element={<Home />} />
-					<Route path="/new" element={<New />} />
+					<Route path="/new" element={<New create={newTask} />} />
+					<Route path="/all" element={<All data={data} />} />
 				</Routes>
 			</div>
 
